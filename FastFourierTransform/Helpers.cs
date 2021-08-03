b836b206950999c3a1ebfbdc4bd5fe2239a73a80
Sebastian Wnęk <seba.wnek@outlook.com>
2021-08-03 15:41:35 +0200

    Working on helpers

--- FastFourierTransform/Helpers.cs
+++ FastFourierTransform/Helpers.cs
@@ -19,5 +19,43 @@ namespace FastFourierTransform
             }
             return false;
         }
+
+        public static ComplexFloat[,] ImportFromRGB(float[,,] data)
+        {
+            int h = data.GetLength(0);
+            int w = data.GetLength(1);
+            double logH = Math.Log2(h);
+            double logW = Math.Log2(w);
+            double roundLogH = Math.Ceiling(logH);
+            double roundLogW = Math.Ceiling(logW);
+            int newH = (int)Math.Pow(2, roundLogH);
+            int newW = (int)Math.Pow(2, roundLogW);
+
+            ComplexFloat[,] array = new ComplexFloat[h, w];
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    array[i, j] = (data[i, j, 0] + data[i, j, 1] + data[i, j, 2])/3;
+                }
+            }
+
+            return array;
+        }
+
+        public static float[,,] ExportToRGB(ComplexFloat[,] data, int width = 0, int heigth = 0)
+        {
+            int h, w;
+            if (width == 0 || heigth == 0)
+            {
+                h = data.GetLength(0);
+                w = data.GetLength(1); 
+            }
+            else
+            {
+                h = heigth;
+                w = width;
+            }
+        }
     }
 }
